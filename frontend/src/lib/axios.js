882c66c7ce202eff9aa67a16a0b516c27d5c7635
Abishek788 <124476937+Abishek788@users.noreply.github.com>
2025-06-21 13:18:39 -0500

    change in the files

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,18 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api",
+//   withCredentials: true,
+// });
+
+import axios from "axios";
+
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5002/api"
+    : process.env.REACT_APP_BACKEND_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api",
+  baseURL,
   withCredentials: true,
 });
